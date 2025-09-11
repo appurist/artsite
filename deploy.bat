adbf08c00ebbea508aef88b99378bb132af90ce1
Paul Whittemore <dev@appurist.com>
2025-09-11 03:24:53 -0300

    Working contents now.

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 @rem or instead of below use: appwrite push function --function-id <FUNCTION_ID>
-npx appwrite push functions --all
+npx appwrite functions create-deployment --function-id=secure-create --code="." --activate
